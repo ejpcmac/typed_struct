7941dc1baae9b5e5c762eab81ef117a056d7feb4
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-06-17 22:15:04 +0200

    Test the type generation

--- test/typed_struct_test.exs
+++ test/typed_struct_test.exs
@@ -1,18 +1,22 @@
 defmodule TypedStructTest do
   use ExUnit.Case
 
-  defmodule TestStruct do
-    use TypedStruct
+  # Store the bytecode so we can get information from it.
+  {:module, _name, bytecode, _exports} =
+    defmodule TestStruct do
+      use TypedStruct
 
-    typedstruct do
-      field :int, integer()
-      field :string, String.t()
-      field :string_with_default, String.t(), default: "default"
-      field :mandatory_int, integer(), enforce: true
+      typedstruct do
+        field :int, integer()
+        field :string, String.t()
+        field :string_with_default, String.t(), default: "default"
+        field :mandatory_int, integer(), enforce: true
+      end
+
+      def enforce_keys, do: @enforce_keys
     end
 
-    def enforce_keys, do: @enforce_keys
-  end
+  @bytecode bytecode
 
   ## Standard cases
 
@@ -27,6 +31,28 @@ defmodule TypedStructTest do
 
   test "enforces keys for fields with `enforce: true`" do
     assert TestStruct.enforce_keys() == [:mandatory_int]
+  end
+
+  test "generates a type for the struct" do
+    # Define a second struct with the type expected for TestStruct.
+    {:module, _name, bytecode2, _exports} =
+      defmodule TestStruct2 do
+        defstruct [:int, :string, :string_with_default, :mandatory_int]
+
+        @type t() :: %__MODULE__{
+                int: integer() | nil,
+                string: String.t() | nil,
+                string_with_default: String.t() | nil,
+                mandatory_int: integer()
+              }
+      end
+
+    # Get both types and standardise them (remove line numbers and rename
+    # the second struct with the name of the first one).
+    type1 = @bytecode |> extract_first_type() |> standardise()
+    type2 = bytecode2 |> extract_first_type() |> standardise()
+
+    assert type1 == type2
   end
 
   ## Problems
@@ -55,4 +81,32 @@ defmodule TypedStructTest do
       end
     end
   end
+
+  ##
+  ## Helpers
+  ##
+
+  defp extract_first_type(bytecode) do
+    bytecode
+    |> Kernel.Typespec.beam_types()
+    |> Keyword.get(:type)
+  end
+
+  defp standardise({name, type, params}) when is_tuple(type),
+    do: {name, standardise(type), params}
+
+  defp standardise({:type, _, type, params}),
+    do: {:type, :line, type, standardise(params)}
+
+  defp standardise({:remote_type, _, params}),
+    do: {:remote_type, :line, standardise(params)}
+
+  defp standardise({:atom, _, TypedStructTest.TestStruct2}),
+    do: {:atom, :line, TypedStructTest.TestStruct}
+
+  defp standardise({type, _, litteral}),
+    do: {type, :line, litteral}
+
+  defp standardise(list) when is_list(list),
+    do: Enum.map(list, &standardise/1)
 end
