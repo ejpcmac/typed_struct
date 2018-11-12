97919c2aab9a77f4fb413c9f9cc39540b6fa0938
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2018-11-13 00:27:18 +0100

    Update the dependencies

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule TypedStruct.MixProject do
     [
       # Development and test dependencies
       {:credo, "~> 0.10.0", only: :dev, runtime: false},
-      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0-rc", only: :dev, runtime: false},
       {:excoveralls, ">= 0.0.0", only: :test, runtime: false},
       {:mix_test_watch, ">= 0.0.0", only: :test, runtime: false},
       {:ex_unit_notifier, ">= 0.0.0", only: :test, runtime: false},
