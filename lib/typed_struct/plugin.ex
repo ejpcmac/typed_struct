e2345370472aa5cf1f2af31616fad08956e23613
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2020-04-22 07:14:33 +0500

    [Plugin API] Fix the init/1 callback not being optional to define

--- lib/typed_struct/plugin.ex
+++ lib/typed_struct/plugin.ex
@@ -12,6 +12,18 @@ defmodule TypedStruct.Plugin do
 
   @callback after_definition(opts :: keyword()) :: Macro.t()
 
-  # All the callbacks are optional so the user has more flexibility.
-  @optional_callbacks [init: 1, field: 3, after_definition: 1]
+  # All the callbacks are optional so the user has more flexibility. Only init/1
+  # is mandatory but an overrideable default is defined when use-ing the module.
+  @optional_callbacks [field: 3, after_definition: 1]
+
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour TypedStruct.Plugin
+
+      @doc false
+      defmacro init(_opts), do: nil
+      defoverridable init: 1
+    end
+  end
 end
