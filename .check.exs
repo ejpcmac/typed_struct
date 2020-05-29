f7f31a6008385434d5802b34880d3942c57bdcc2
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2020-05-29 11:13:51 +0500

    [CI] Run the formatter only for last Elixir version

--- .check.exs
+++ .check.exs
@@ -1,7 +1,20 @@
+# Check if the version used by the runner is currently the last supported
+# version, that is: the version used in the Nix shell on development machines.
+last_supported_version? =
+  System.version()
+  |> Version.parse!()
+  |> Version.match?("~> 1.8.0")
+
 [
   skipped: false,
   tools: [
     {:compiler, "mix compile --force --verbose --warnings-as-errors"},
-    {:ex_unit, "mix test --trace"}
+    {:ex_unit, "mix test --trace"},
+
+    # Run the formatter and ex_doc only for the last supported version. This
+    # avoids errors in CI when the formatting changes or ex_doc is not
+    # compatible with an old Elixir version.
+    {:formatter, last_supported_version?},
+    {:ex_doc, last_supported_version?}
   ]
 ]
