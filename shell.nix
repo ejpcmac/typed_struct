907a56927ee70d784953d116951c1e42aca54d10
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2021-10-26 20:32:43 +0200

    build(nix): relaxe the Elixir version

--- shell.nix
+++ shell.nix
@@ -4,13 +4,10 @@ with pkgs;
 
 let
   inherit (lib) optional optionals;
-
-  elixir = beam.packages.erlangR22.elixir_1_10;
-  gitflow = gitAndTools.gitflow;
 in
 
 mkShell {
-  buildInputs = [ elixir git gitflow ]
+  buildInputs = [ elixir git gitAndTools.gitflow ]
     ++ optional stdenv.isLinux libnotify # For ExUnit Notifier on Linux.
     ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
     ++ optional stdenv.isDarwin terminal-notifier # For ExUnit Notifier on macOS.
