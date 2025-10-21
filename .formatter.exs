131f2020fa98aef57a0dbf5b4c965f0403e73886
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2025-10-21 13:07:09 +0200

    env(formatter): format the `.check.exs`

--- .formatter.exs
+++ .formatter.exs
@@ -2,7 +2,7 @@ locals_without_parens = [field: 2, field: 3, plugin: 1, plugin: 2]
 
 [
   inputs: [
-    "{mix,.iex,.formatter,.credo}.exs",
+    "{mix,.check,.credo,.formatter,.iex}.exs",
     "{config,lib,test}/**/*.{ex,exs}"
   ],
   line_length: 80,
