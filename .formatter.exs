ce1b604715380f38915e303fcad04832a395ce7c
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2025-10-22 21:44:49 +0200

    ci(scripts): add a script to check commits

--- .formatter.exs
+++ .formatter.exs
@@ -3,7 +3,7 @@ locals_without_parens = [field: 2, field: 3, plugin: 1, plugin: 2]
 [
   inputs: [
     "{mix,.check,.credo,.formatter,.iex}.exs",
-    "{config,lib,test}/**/*.{ex,exs}"
+    "{config,lib,scripts,test}/**/*.{ex,exs}"
   ],
   line_length: 80,
   locals_without_parens: locals_without_parens,
